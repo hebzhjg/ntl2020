f0dfd76e4c94be53ee178dea4d4257a96c78a731
hebzhjg@qq.com <hebzhjg@qq.com>
2019-12-13 20:56:12 +0800

    有两个文件没有提交，补充

---
+++ code/xdclass_springboot/src/main/java/net/xdclass/XdclassApplication.java
@@ -0,0 +1,14 @@
+package net.xdclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdclassApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(XdclassApplication.class, args);
+    }
+
+}
